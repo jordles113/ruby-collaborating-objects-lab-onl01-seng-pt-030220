e732c60cca5bea44e8b2da9a48957cbacfd24f6c
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-28 22:30:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,19 +1,29 @@
 class Song
   attr_accessor :name, :artist
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
-  def self.new_by_filename(filename)
-    artist, song = filename.split(" - ")
-    new_song = self.new(song)
-    new_song.artist_name = artist
-    new_song
+  def self.all
+    @@all
   end
+
+  def artist_name
+    artist.name if artist
+  end
+
+  def self.new_by_filename(file)
+    name = file.gsub(".mp3","").split(" - ")
+    s = Song.new(name[1])
+    s.artist_name = name[0]
+    s
+  end  
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
-    self.artist.add_song(self)
   end
 end
